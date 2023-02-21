20b010595409bda4a41eda73860b9446f774c905
Nahuel <117119383+acostanahuel@users.noreply.github.com>
2023-02-21 19:58:08 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
 import express  from "express";
-import productsRouter from "./routes/products.router.js"
-import cartRouter from "./routes/cart.router.js"
-import handlebars from "express-handlebars"
-import {Server} from "socket.io"
-import ProductManager from './ProductManager.js';
+import productsRouter from "./routes/products.router.js";
+import cartRouter from "./routes/cart.router.js";
+import handlebars from "express-handlebars";
+import ProductManager from "./ProductManager.js";
+import viewRouter from "./routes/views.router.js";
+import {Server} from "socket.io";
+import __dirname from "./util.js";
+
 
 
 
 const productManager = new ProductManager();
 const products = await productManager.getProducts();
 
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use (express.static(__dirname+`/public`));
 
 //Handlebars
 //Uso de vista de plantillas
 app.engine (`handlebars`, handlebars.engine());
-// app.set('views', __dirname + "/views");
+app.set('views', __dirname + "/views");
 app.set('view engine', 'handlebars');       //nos permite usar response.render() el handlebars
 
 
@@ -26,17 +31,23 @@ app.set('view engine', 'handlebars');       //nos permite usar response.render()
 ///delegamos, le decimos a app que use el router
 app.use(`/api/products`, productsRouter); 
 app.use(`/api/cart`, cartRouter);  
+app.use(`/api/views`, viewRouter);
 
-app.get(`/`,(req, res)=>{
-    res.render('home',{products:products[11]})
-  })
+// console.log(products);
+const SERVER_PORT = 8080;
 
-console.log(products);
-const SERVER_PORT = 8081;
 
-const httpServer = app.listen(8081, () => {
+const httpServer = app.listen(8080, () => {
     console.log("Server listening in " + SERVER_PORT);
     // console.log(__dirname + "/views");
 });
 
-const socketServer = new Server(httpServer);
+const socketServer = new Server (httpServer);
+
+socketServer.on('connection', socket => {
+  console.log("nuevo cliente conectado");
+
+  socket.on (`message`, data => {
+    console.log(data);
+  });
+});
