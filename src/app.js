a26dfab06bd6c65c69c36eae3e23f2579becec1c
Nahuel <117119383+acostanahuel@users.noreply.github.com>
2023-02-27 17:07:55 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -65,3 +65,26 @@ socketServer.on('connection', (socket) => {
   });
 });
    
+
+
+
+//o también tengo esta opcion 2
+const socket = io();
+socket.emit("client:message", 'HI I AM A NEW USER CONNECTED ')
+
+socket.emit('client:products', "requier the products")
+
+socket.on('server:productsUp', (data) => {
+  console.log(data)
+  socket.emit('client:productsUpdate', data )
+});
+
+socket.on('server:updateProducts', (data)=> {
+  if(data){
+    console.log("products al dia")
+    
+  } else {
+    console.log("products para actualizar")
+
+  }
+})
