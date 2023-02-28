5053bda148dd856e494a7bf79dbc8af5616639de
Nahuel <117119383+acostanahuel@users.noreply.github.com>
2023-02-28 18:17:38 -0300

    Update index.js

--- src/public/index.js
+++ src/public/index.js
@@ -1,33 +1,32 @@
+const productsContainer = document.querySelector('#products-container');
 const socket = io();
+socket.emit('client:message', 'HI I AM A NEW USER CONNECTED');
 
-socket.on('updated-products', (products) => {
-  // Aquí puedes actualizar la lista de productos en el cliente con la lista actualizada enviada desde el servidor
-  console.log('Productos actualizados:', products);
+setInterval(() => {
+  socket.emit('client:products');}, 3000);
+
+socket.on('server:products', async (Products) => {
+  addProductsToRealTime(Products);
 });
 
-
-///o tengo también esta opcion 2
-const socketServer = new Server(httpServer);
-
-socketServer.on('connection', socket => {
-  console.log("new user conected:", socket.id);
-
-  socket.on("client:message", data => {
-    console.log("-------------------------------")
-    console.log(data);
+const addProductsToRealTime = (products) => {
+  let html = '';
+  products.forEach(p => {
+    html += `
+    <div class="col-lg-4 d-flex justify-content-center align-item-center p-5">
+    <div class="card shadow" style="width: 18rem;">
+      <img src="${p.thumbnail}" class="card-img-top" alt="thumbnail: ${p.thumbnail}">
+      <div class="card-body">
+        <h5 class="card-title">Title: ${p.title}</h5>
+        <p class="card-text">Description: ${p.description}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Price: ${p.price}</li>
+        <li class="list-group-item">Status: ${p.status}</li>
+        <li class="list-group-item">Category: ${p.category}</li>
+      </ul>
+    </div>
+  </div>`
+    productsContainer.innerHTML = html;
   });
-
-  socket.on('client:products', async (data) => {
-    console.log(data)
-    socket.emit('server:productsUp', await productManager.products)
-
-    socket.on('client:productsUpdate', async (data) => {
-      if (JSON.stringify(data) !== JSON.stringify(await productManager.products)) {
-      socket.emit('server:updateProducts', false);
-      } else {
-        console.log("Productos iguales, no hay actualizaciones")
-        socket.emit('server:updateProducts', true)
-      }
-    });
-  });
-});
+};
